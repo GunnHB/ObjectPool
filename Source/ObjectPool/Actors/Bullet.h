99209c5ada5f7e904af8bbb3fb838c00611b8fbd
DESKTOP-OAPN4UI\Gunn <gunn0467@gmail.com>
2025-08-04 16:03:37 +0900

    [ObjectPool] Bullet 액터 추가

---
+++ Source/ObjectPool/Actors/Bullet.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Bullet.generated.h"
+
+UCLASS()
+class OBJECTPOOL_API ABullet : public AActor
+{
+	GENERATED_BODY()
+};
