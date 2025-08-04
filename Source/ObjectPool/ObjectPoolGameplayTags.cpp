98239763b60a5ff914970f5fb91baaff3ead2de9
DESKTOP-OAPN4UI\Gunn <gunn0467@gmail.com>
2025-08-04 15:43:33 +0900

    [ObjectPool] ObjectPoolGameplayTags.h 추가

---
+++ Source/ObjectPool/ObjectPoolGameplayTags.cpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace ObjectPoolGameplayTags
+{
+	OBJECTPOOL_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(ObjectPool_Bullet);
+	OBJECTPOOL_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(ObjectPool_Monster);
+}
