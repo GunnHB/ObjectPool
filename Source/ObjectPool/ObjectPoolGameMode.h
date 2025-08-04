7868b1b550b873cde57c3b7c2698dce3a9a542fa
DESKTOP-OAPN4UI\Gunn <gunn0467@gmail.com>
2025-08-04 13:43:28 +0900

    [ObjectPool] 프로젝트 커밋

---
+++ Source/ObjectPool/ObjectPoolGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ObjectPoolGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AObjectPoolGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AObjectPoolGameMode();
+};
+
+
+
